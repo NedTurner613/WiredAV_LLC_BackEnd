cdbafc852712abc1e8824830eec1a3e904e722aa
Ned <nedturner613@gmail.com>
2026-08-01 13:40:34 -0500

    added base app

---
+++ app/src/test/java/com/wiredav/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiredav.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
