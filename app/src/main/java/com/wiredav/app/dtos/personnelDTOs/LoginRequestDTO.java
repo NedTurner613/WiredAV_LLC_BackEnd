1fa306760a7437f608ec52ea8ffbfd95191e7bd9
ermonio <moni.osborne07@gmail.com>
2026-08-17 09:31:31 -0500

    Delete getter and setter annotations

--- app/src/main/java/com/wiredav/app/dtos/personnelDTOs/LoginRequestDTO.java
+++ app/src/main/java/com/wiredav/app/dtos/personnelDTOs/LoginRequestDTO.java
@@ -1,12 +1,6 @@
 package com.wiredav.app.dtos.personnelDTOs;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public class LoginRequestDTO {
     private String email;
     private String password;
--- app/src/main/java/com/wiredav/app/dtos/personnelDTOs/LoginResponseDTO.java
+++ app/src/main/java/com/wiredav/app/dtos/personnelDTOs/LoginResponseDTO.java
@@ -1,14 +1,6 @@
 package com.wiredav.app.dtos.personnelDTOs;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoginResponseDTO {
     private String token;
     private Long userId;
